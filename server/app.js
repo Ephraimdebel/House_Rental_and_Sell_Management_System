956b0b7ccc862675f501e34f876e994dcf1b2090
JiruGutema <jethior1@gmail.com>
2024-12-28 17:06:22 +0300

    filtering is added for the house controller. update house functionalities are updated with the input handling and the house_routes is renamed to house_router

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ const app = express();
 const port = process.env.PORT || 5500;
 const dbConnection = require("./config/db");
 const cors = require("cors");
-const houseRoute = require("./app/routes/house_routes");
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 //user routes middleware
 const userRoute = require("./app/routes/user_router");
 app.use("/api/users", userRoute);
+// house routes middleware
+const houseRoute = require("./app/routes/house_router");
 app.use("/api", houseRoute);
 
 async function start() {
--- server/app/routes/house_router.js
+++ server/app/routes/house_router.js
@@ -6,12 +6,14 @@ const {
   addHouse,
   deleteHouse,
   updateHouse,
+  filter,
 } = require("../controllers/house_controller");
 
 router.get("/house", getHouse);
 router.post("/addHouse", addHouse);
 router.delete("/deleteHouse/:houseId", deleteHouse);
 router.put("/updateHouse/:houseId", updateHouse);
+router.get("/filter", filter);
 
 module.exports = router;
 router.get("/house", getHouse);
