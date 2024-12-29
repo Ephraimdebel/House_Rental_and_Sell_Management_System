8d69d3604e9422aef7d3ebd31a00b24507763d8d
JiruGutema <jethior1@gmail.com>
2024-12-29 15:30:13 +0300

    i have no idea what s3 section is doing! it is not complete. was implemented in mongodb and to refactor later.

--- server/app/routes/user_router.js
+++ server/app/routes/user_router.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/user_controller");
 
-const {getHouse, addHouse, deleteHouse, updateHouse} = require("../controllers/house_controller");
-
 router.post("/register", register);
 router.post("/login", login);
 
