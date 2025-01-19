29f53d79dde55fd010b3b0d570c5093a7059d0cb
Ephraimdebel <ephraimdebel@gmail.com>
2025-01-20 01:20:34 +0300

    notfication back end and property display added

--- server/app/routes/house.routes.js
+++ server/app/routes/house.routes.js
@@ -7,14 +7,18 @@ const {
   getAllListings,
   getListingsByType,
   getFilteredHouses,
+  deleteProperty,
+  getAllListingsNew,
 } = require("../controllers/house.controller");
 
 const authMiddleware = require("../middlewares/auth_middle_ware");
 const upload = require("../../utils/uploads");
 
 router.get("/house", getAllListings);
+router.get("/houses", getAllListingsNew);
 router.post("/addHouse",upload.array('photos',5),authMiddleware, addHouse);
 router.get("/house/:id", getHouseDetails)
+router.delete("/house/:id",authMiddleware, deleteProperty)
 router.get("/housetype", getListingsByType)
 router.get("/filteredhouse", getFilteredHouses)
 
